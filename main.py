5951eb94dac08af5066fba8fc1636cd91930eec6
Akif245 <abdulakif44570@gmail.com>
2025-05-28 00:02:26 +0530

    Flask

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "He AI!"
+@app.route('/bye')
+def bye():
+    return "Nikal..........."
+
+@app.route('/<name>')
+def greet(name):
+    return f"Welcome {name}!"
 
 
+if __name__=='__main__':
+    app.run(debug=True)
