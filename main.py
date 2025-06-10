be2f1bf39baa4fcb73ed4ad7189aa508d83abb13
Akif245 <abdulakif44570@gmail.com>
2025-06-11 00:18:14 +0530

    simple_profile_flask

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "He AI!"
-@app.route('/bye')
-def bye():
-    return "Nikal..........."
-
-@app.route('/<name>')
-def greet(name):
-    return f"Welcome {name}!"
-
+        return render_template("akif.html")
+    # return '<h1 style="text-align:center">Hello World!</h1>' \
+    #        '<img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmdqcjBjOTBlOTl2bGR6aXhlc2VkdW5yemhvZHp1dncxYWtsamF1ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Puc4FZWExJc0E/giphy.gif ">'
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True  )
